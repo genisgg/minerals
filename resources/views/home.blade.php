d1005a9a4b8a7ac16b9f8542ecdb047e3a87dcfa
genisgg <genis.gallifa@gmail.com>
2024-12-06 12:42:35 +0100

    commit_6-12

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<x-guest-layout>
+
+    <div class="py-5">
+        <div class="container max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{__('Home page')}}
+            </h2>
+            <section>
+                <p>
+                    Lorem ipsum awildalkahba adskjfhasdf
+                </p>
+            </section>
+
+        </div>
+    </div>
+
+</x-guest-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-20 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
@@ -16,10 +16,18 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                @auth
+                    
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -50,6 +58,8 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                @endauth
+
             </div>
 
             <!-- Hamburger -->
@@ -70,8 +80,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                {{ __('Home') }}
+            </x-responsive-nav-link>
         </div>
-
+@auth
+    
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
@@ -96,5 +110,7 @@
                 </form>
             </div>
         </div>
+        @endauth
+
     </div>
 </nav>
