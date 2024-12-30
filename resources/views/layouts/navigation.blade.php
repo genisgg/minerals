f1a1c3e2af7f0b236da7a934e53dad5fa3b81648
genisgg <genis.gallifa@gmail.com>
2024-12-30 19:59:45 +0100

    iniciIdioma

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -70,7 +70,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Perfil') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -80,9 +80,14 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('Tancar sessió') }}
                             </x-dropdown-link>
                         </form>
+
+                        <x-dropdown-link>
+                            {{ __('Idioma') }}
+                        </x-dropdown-link>
+                        
                     </x-slot>
                 </x-dropdown>
                 @endauth
